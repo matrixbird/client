ba7aa39a9e5c772c5eb7933caeeb66c3532a69e6
hummingbard <mail@hummingbard.com>
2025-04-04 05:41:41 +0600

    floating compose

---
+++ src/lib/editor/mobile-new-message.svelte
@@ -0,0 +1,43 @@
+<script>
+import { plus } from '$lib/assets/icons'
+import { createEditorStore } from '$lib/store/editor.svelte'
+const store = createEditorStore()
+
+import { ui_state } from '$lib/store/app.svelte'
+let expanded = $derived(ui_state?.expanded)
+
+function newMessage() {
+    store.create()
+}
+
+</script>
+
+<div class="new-message">
+
+    <button 
+        onclick={newMessage}
+        class="primary flex justify-center items-center py-3 px-3 w-full">
+
+        <div class="flex">
+            <div class="">
+                {@html plus}
+            </div>
+            <div class="mx-2">
+                Compose
+            </div>
+        </div>
+
+    </button>
+</div>
+
+
+<style lang="postcss">
+
+.new-message {
+    position: fixed;
+    z-index: 100;
+    bottom: 2rem;
+    right: 1.5rem;
+}
+
+</style>
