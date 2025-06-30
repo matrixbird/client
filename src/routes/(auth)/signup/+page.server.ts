15de321a1b1090e8a6356d38f1d1c65341b6a760
hummingbard <mail@hummingbard.com>
2025-06-30 07:47:13 +0600

    error on appservice failure

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -1,23 +1,35 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ parent, fetch, url }) => {
 
     const { MATRIXBIRD_SERVER } = await parent();
 
     const endpoint = `${MATRIXBIRD_SERVER}/features/authentication`;
-    const res = await fetch(endpoint);
-    const data = await res.json();
 
-    const code = url.searchParams.get('code')
-    if(code) {
-        const endpoint = `${MATRIXBIRD_SERVER}/auth/code/validate/${code}`;
-        const res = await fetch(endpoint);
-        const json = await res.json();
-        if(json?.valid) {
-            json.code = code;
-            data.invite_code = json;
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw new Error('Failed to fetch authentication features');
         }
-    }
+        const data = await response.json();
 
-    return data;
+        const code = url.searchParams.get('code')
+        if(code) {
+            const endpoint = `${MATRIXBIRD_SERVER}/auth/code/validate/${code}`;
+            const res = await fetch(endpoint);
+            const json = await res.json();
+            if(json?.valid) {
+                json.code = code;
+                data.invite_code = json;
+            }
+        }
+
+        return data;
+    } catch (err) {
+        console.error("Error:", err);
+        error(500, {
+            message: err?.message,
+        });
+    }
 
 }
